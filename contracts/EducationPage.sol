6a5043fb5829de1602548fce7e80b6587cc502d9
kavimanik <kavimanik89@gmail.com>
2023-11-09 16:15:51 -0700

    working smart contract on blockchain

--- contracts/EducationPage.sol
+++ contracts/EducationPage.sol
@@ -1,5 +1,25 @@
 pragma solidity 0.5.16;
 
 contract EducationPage{
-    uint public contractCount = 0;
+    uint public assignmentCount = 0;
+
+  struct Assignment {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Assignment) public assignments;
+
+  constructor() public {
+    createAssignment("New Assignment Created");
+  }
+
+  function createAssignment(string memory _content) public {
+    assignmentCount++;
+    assignments[assignmentCount] = Assignment(assignmentCount, _content, false);
+  }
+
+
+
 }
